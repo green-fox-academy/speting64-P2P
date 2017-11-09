8baaa20279fbc0fde23ea5cf147859041fec8b37
Viktor Szabo <viklukacs64@gmail.com>
2017-11-09 17:51:00 +0100

    FIrst commit

---
+++ src/main/java/com/greenfox/chatapp/controller/MessageController.java
@@ -0,0 +1,16 @@
+package com.greenfox.chatapp.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+    @RequestMapping("/")
+    public String mainPage(){
+        return "main";
+    }
+
+
+
+}
---
+++ src/main/java/com/greenfox/chatapp/model/Message.java
@@ -0,0 +1,44 @@
+package com.greenfox.chatapp.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    long id;
+    String userName;
+    String message;
+
+    public Message(){
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
